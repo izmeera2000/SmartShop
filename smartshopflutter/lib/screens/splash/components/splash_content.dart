2372c0691481249d123d85425f30780f2d9d3767
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2025-04-24 01:17:58 +0800

    ASDSA
    DSADA

--- smartshopflutter/lib/screens/splash/components/splash_content.dart
+++ smartshopflutter/lib/screens/splash/components/splash_content.dart
@@ -21,7 +21,7 @@ class _SplashContentState extends State<SplashContent> {
       children: <Widget>[
         const Spacer(),
         const Text(
-          "SmartShop",
+          "SMART SHOP",
           style: TextStyle(
             fontSize: 32,
             color: kPrimaryColor,
--- smartshopflutter/lib/screens/splash/splash_screen.dart
+++ smartshopflutter/lib/screens/splash/splash_screen.dart
@@ -17,7 +17,7 @@ class _SplashScreenState extends State<SplashScreen> {
   int currentPage = 0;
   List<Map<String, String>> splashData = [
     {
-      "text": "Welcome to SmartShop, Let’s shop!",
+      "text": "Welcome to Smart Shop, Let’s shop!",
       "image": "assets/images/splash_1.png"
     },
     {
