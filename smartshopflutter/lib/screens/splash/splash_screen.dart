ad94cc8d37ea13ed3782220f637413ae4bb0878c
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2025-04-24 01:13:42 +0800

    asd
    asdasd

--- smartshopflutter/lib/screens/splash/splash_screen.dart
+++ smartshopflutter/lib/screens/splash/splash_screen.dart
@@ -17,12 +17,12 @@ class _SplashScreenState extends State<SplashScreen> {
   int currentPage = 0;
   List<Map<String, String>> splashData = [
     {
-      "text": "Welcome to Tokoto, Let’s shop!",
+      "text": "Welcome to SmartShop, Let’s shop!",
       "image": "assets/images/splash_1.png"
     },
     {
       "text":
-          "We help people conect with store \naround United State of America",
+          "We help people conect with store \naround ADTEC Taiping",
       "image": "assets/images/splash_2.png"
     },
     {
